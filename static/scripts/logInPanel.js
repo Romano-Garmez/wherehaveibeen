b2193e4c87ee5ceaa70289cc05ac4f1257943747
roman garms <romangarms@gmail.com>
2024-08-17 00:19:50 -0700

    Revert "trying to fix how the form works"
    This reverts commit b2e55a27831eb3328e75028eded608b60b960652.

--- static/scripts/logInPanel.js
+++
@@ -1,8 +0,0 @@
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-    document.getElementById("sign_out").style.display = "none"
-}
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-    document.getElementById("sign_out").style.display = "block";
-}
