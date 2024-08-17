b2e55a27831eb3328e75028eded608b60b960652
roman garms <romangarms@gmail.com>
2024-08-16 19:52:25 -0700

    trying to fix how the form works

---
+++ static/scripts/logInPanel.js
@@ -0,0 +1,8 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    document.getElementById("sign_out").style.display = "none"
+}
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("sign_out").style.display = "block";
+}
